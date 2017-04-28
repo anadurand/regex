5b309e622bfe0ad9fe694461d44df1f72118a714
Ana Durand <ana.dc1004@gmail.com>
2017-04-28 10:51:39 -0500

    validaciones

--- main.js
+++ main.js
@@ -12,19 +12,47 @@ var textName = "/[A-Z]{1,}[a-z]+/";
 
 function createTooltip(mensaje){
   var span = document.createElement("span");
-  span.setAttribute("class","tool");
   span.innerHTML = mensaje;
   return span;
 }
 
+//Remueve los tooltip
+function quitTooltip(nodo) {
+  if(nodo.nextElementSibling != null){
+    nodo.parentNode.removeChild(nodo.nextSibling);
+  }
+}
+//cambia el texto del tooltip
+function changeMessage(nodo,text){
+  if(nodo.nextElementSibling == null){
+    nodo.parentNode.appendChild(createTooltip(text));
+  }else{
+    nodo.nextElementSibling.textContent = text;
+
+  }
+}
 function validarForm(){
   var c = 0;
-  if(name.value != "" && name.value.match(testName)){
-    c++;
+
+
+}
+
+function validarFullName(nodo){
+  
+  if(nodo.value == ""){
+    if(nodo.nextElementSibling == null){
+      divBox[0].appendChild(createTooltip("Falta ingresar su nombre"));
+    }
+
   }else{
-    name.focus();
+    if(!nodo.value.match(testName)){
+      mens = "La primera letra debe ser mayúscula";
+      change(name,mens);
+      nodo.focus();
+    }else{
+      quit(name);
+
+    }
   }
-  if(fullName.value != "" && fullName.value.match(testName)){
-    c++;
-  }else{ fullName.focus();}
-  if()
+
+}
