8ea1b69febfe1ba0506a1695de5654d6342c1ec0
Ana Durand <ana.dc1004@gmail.com>
2017-04-28 11:12:36 -0500

    refactorizando

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-var date = document.getElementById("date");
 var password = document.getElementById("password");
 var confirmP = document.getElementById("confirmP");
 var enviar = document.getElementById("enviar");
-var c = 0;
 enviar.addEventListener("click", function(e){
 //  e.preventDefault();
   if(password.value == confirmP.value){
-    c++;
   }else{
     alert("Confirmar contraseña no es valido");
     confirmP.focus();
   }
-
-  console.log(date.value);
 
 });
